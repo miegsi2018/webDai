9c61f517ae8d93023397401ef59355c03394fe04
naishes <32779815+naishes@users.noreply.github.com>
2018-02-27 01:29:42 +0000

    tudo pronto

---
+++ controllers/index.route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const model = require('../models/user.model');
+
+router.get('/', function(request, response){
+	//console.log(request.isAuthenticated());
+
+	response.set("Content-Type", "text/html");
+	response.render('./index', {
+	})
+
+
+});
+module.exports = router;
