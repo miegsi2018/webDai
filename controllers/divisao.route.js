a2a15aaf8e67d85bc06379bf40c464ad0d2b03a6
naishes <32779815+naishes@users.noreply.github.com>
2018-04-06 17:04:05 +0100

    we

--- controllers/divisao.route.js
+++ controllers/divisao.route.js
@@ -5,18 +5,10 @@ router.get('/', function(request, response){
 	//console.log(request.isAuthenticated());
 
 	response.set("Content-Type", "text/html");
-	response.render('./room', {
+	response.render('./divisao', {
 	})
 
 
 });
-router.get('/sensor', function(request, response){
-	//console.log(request.isAuthenticated());
 
-	response.set("Content-Type", "text/html");
-	response.render('./sensor', {
-	})
-
-
-});
 module.exports = router;
