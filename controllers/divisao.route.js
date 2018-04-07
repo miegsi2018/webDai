a5c329ffe37dec150a045a09aeb4db170b15c937
Rui Marques <a.marques1967@gmail.com>
2018-04-07 12:03:16 +0100

    Visão por divisão individual v0.1
    Routas definidas para se ver os sensores de cada divisão individualmente, falta agora a página dos sensores

--- controllers/divisao.route.js
+++ controllers/divisao.route.js
@@ -1,73 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const model = require('../models/user.model');
+const model = require('../models/divisao.model');
 var mqtt = require('mqtt')
 
 router.get('/', function(request, response){
-	//console.log(request.isAuthenticated());
-
   var id = request.user.email;
-  var divisoesUser = new Array();
-  model.readEmail(id, function(divisoes) {
+  
+  model.readEmail(id, function(divisoes){  
+    response.set("Content-Type", "text/html");
+	  response.render('./divisao', {
+      divisoes : divisoes
+	  })
+  })
+});
 
-    console.log('##################################################' + divisoes[0].email + '########');
+router.get('/room/:id_divisao', function(request, response){
+  var id = request.user.email;
 
-    /*for (var e of divisoesUser) {
-       console.log(e.divisao)
-      }
-	*/
-    var client = mqtt.connect('mqtt://94.61.10.49:80', {
-      username: "dai",
-      password: '12345678'
-    })
-
-
-
-    if (divisoes.length > 1) {
-      console.log('###############///////////////////////###################(((((((()))))))))))');
-
-      client.on('connect', function() {
-        console.log('MQTT IS WORKING' + ' ' + 2)
-
-        for (var e of divisoes) {
-          client.subscribe('dai/' + e.divisao)
-        }
-        client.publish('presence', 'Hello mqtt')
-
-
-
-
-      })
-    } else {
-      client.on('connect', function() {
-        console.log('MQTT IS WORKING' + ' ' + 2)
-        client.subscribe('dai/' + divisoes[0].divisao)
-        client.publish('presence', 'Hello mqtt')
+  model.readDivisao(request.params.id_divisao, function(divisao){
+    if(divisao != undefined){
+      response.set("Content-Type", "text/html");
+      response.render('./sensor', {
+        sensores : sensores
       })
     }
-    io.on('connection', function(socket) {
-
-      client.on('message', (topic, message) => {
-
-
-
-        console.log(`Received message: '${message}'`);
-        socket.emit(topic, message.toString());
-        var labels = JSON.parse(message);
-        console.log(labels)
-
-
-
-
-      });
-    });
-	response.set("Content-Type", "text/html");
-	response.render('./divisao', {
-		divisoes : divisoes
-	})
-
-
-});
+  })
 });
 
 module.exports = router;
--- models/divisao.model.js
+++ models/divisao.model.js
@@ -1,33 +1,41 @@
 module.exports = {
 	listaDivisao(callback) {
-		var sql = 'SELECT * FROM dwpt_dai.divisao';
+		var sql = 'SELECT * FROM dwpt_dai.contas_v';
 		global.connection.query(sql, function(error, rows, fields){
 			if (error) throw error;
 			callback(rows);
 		});
 	},
 
-	readSensor(id_sensor, callback) {
-		var sql = "SELECT * from dwpt_dai.sensor where id_sensor=?";	
-		global.connection.query(sql, [id_sensor], function(error, rows, fields) {
-			if (error) throw error;
-			callback(rows[0]);			
-		});
-	},	
-
-	createSensor(data, callback) {
-		var sql = "INSERT INTO dwpt_dai.sensor (id_sensor, id_tipo) VALUES (?,?)"; 
-		global.connection.query(
-			sql, [data.id_sensor, data.id_tipo], function(error, rows, fields) {
+	readDivisao(id_divisao, callback) {
+		var sql = "SELECT * FROM dwpt_dai.contas_v where id_divisao=?";	
+		global.connection.query(sql, [id_divisao], function(error, rows, fields) {
 			if (error) throw error;
 			callback(rows[0]);			
 		});
 	},
 
-	update(username, data, callback) {
-		var sql = "UPDATE users SET nome=?,password=? WHERE username=?"; 
+	readEmail(email, callback) {
+		var sql = 'SELECT * FROM dwpt_dai.contas_v where email=?';
+		global.connection.query(sql, [email], function(error, rows, fields) {
+		  if (error) throw error;
+		  callback(rows);
+		});
+	},	
+
+	createDivisao(data, callback) {
+		var sql = "INSERT INTO dwpt_dai.divisao (id_casa, nome, id_sensor) VALUES (?,?,?)"; 
 		global.connection.query(
-			sql, [data.nome, data.password, username], function(error, rows, fields) {
+			sql, [data.id_casa, data.nome, data.id_sensor], function(error, rows, fields) {
+			if (error) throw error;
+			callback(rows[0]);			
+		});
+	},
+
+	updateDivisao(id_divisao, data, callback) {
+		var sql = "UPDATE divisao SET id_casa=?,nome=?,id_sensor=? WHERE id_divisao=?"; 
+		global.connection.query(
+			sql, [data.id_casa, data.nome, id_divisao], function(error, rows, fields) {
 			if (error) throw error;
 			callback(rows[0]);			
 		});
