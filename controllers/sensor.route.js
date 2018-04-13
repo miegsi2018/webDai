d33beb080a77fd308a9ca3a3ba43fa894f26c305
naishes <32779815+naishes@users.noreply.github.com>
2018-04-13 02:06:10 +0100

    jhk

--- controllers/sensor.route.js
+++ controllers/sensor.route.js
@@ -2,71 +2,25 @@ const express = require('express');
 const router = express.Router();
 const model = require('../models/user.model');
 var mqtt = require('mqtt')
+const sensorModel = require('../models/sensor.model');
+const divisaoModel = require('../models/divisao.model');
 
 
 
-router.get('/', function(request, response) {
-  //console.log(request.isAuthenticated()); 
-
-
+router.get('/', function(request, response){
+  //console.log(request.isAuthenticated());
   var id = request.user.email;
-  var sensoresUser = new Array();
-  model.readEmail(id, function(sensores) {
-
-    console.log('##################################################' + sensores[0].email + '########');
-
-    var client = mqtt.connect('mqtt://94.61.10.49:80', {
-      username: "dai",
-      password: '12345678'
-    })
-
-
-
-    if (sensores.length > 1) {
-      console.log('###############///////////////////////###################(((((((()))))))))))');
-
-      client.on('connect', function() {
-        console.log('MQTT IS WORKING' + ' ' + 2)
-
-        for (var e of sensores) {
-          client.subscribe('dai/' + e.sensor)
-        }
-        client.publish('presence', 'Hello mqtt')
-
-
-
-
-      })
-    } else {
-      client.on('connect', function() {
-        console.log('MQTT IS WORKING' + ' ' + 2)
-        client.subscribe('dai/' + sensores[0].sensor)
-        client.publish('presence', 'Hello mqtt')
-      })
-    }
-    io.on('connection', function(socket) {
-
-      client.on('message', (topic, message) => {
-
-
-
-        console.log(`Received message: '${message}'`);
-        socket.emit(topic, message.toString());
-        var labels = JSON.parse(message);
-        console.log(labels)
-
-
-
-
-      });
-    });
-
-    response.set("Content-Type", "text/html");
-    response.render('./sensor', {
-      sensores: sensores
-
-    })
-  });
+  
+  divisaoModel.readEmail(id, function(divisoes){  
+    sensorModel.listaSensor(function(sensor) {
+	response.set("Content-Type", "text/html");
+	response.render('./adicionar_sensor', {
+        sensor : sensor,
+        divisoes : divisoes
+	})
+})
+})
+	
 });
 
 module.exports = router;
