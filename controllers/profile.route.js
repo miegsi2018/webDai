191bef626e46eefce684e1b134473868516c98f0
Rui Marques <a.marques1967@gmail.com>
2018-04-14 09:20:42 +0100

    Barra u suck

--- controllers/profile.route.js
+++ controllers/profile.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const sensorModel = require('../models/sensor.model');
+const divisaoModel = require('../models/divisao.model');
 
 router.get('/', function(request, response){
 	//console.log(request.isAuthenticated());
@@ -12,11 +14,16 @@ router.get('/', function(request, response){
 });
 router.get('/create', function(request, response){
 	//console.log(request.isAuthenticated());
-
+	divisaoModel.readEmail(id, function(divisoes){  
+		sensorModel.listaSensor(function(sensor) {
 	response.set("Content-Type", "text/html");
 	response.render('./create_user', {
+		divisoes : divisoes,
+		sensor : sensor
 	})
+})
+})
 
 
 });
-module.exports = router;
+module.exports = router; 
