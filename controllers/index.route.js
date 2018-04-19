2cc3f42d4842239034f0f3c02b699a2dc59868c0
vitormiegsi <vhoa1995@hotmail.com>
2018-04-19 03:04:53 +0100

    ss

--- controllers/index.route.js
+++ controllers/index.route.js
@@ -25,11 +25,11 @@ router.get('/home', function(request, response) {
 
   var id = request.user.email;
 
-req.get('http://localhost:8080/view/'+ id, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+  req.get('http://localhost:8080/view/' + id, function(error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+  });
 
 
 
