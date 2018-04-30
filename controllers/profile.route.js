a26f7a20d0b8e9cfdb020eb6f99fc3c198e68829
naishes <32779815+naishes@users.noreply.github.com>
2018-04-30 02:55:04 +0100

    timeLine and things

--- controllers/profile.route.js
+++ controllers/profile.route.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 const sensorModel = require('../models/sensor.model');
 const divisaoModel = require('../models/divisao.model');
+const req = require('request');
 
 router.get('/', function(request, response){
 	//console.log(request.isAuthenticated());
-
+	var id = request.user.email;
+	req.get('http://localhost:8080/utilizador', function(error, resp, body) {
+		req.get('http://localhost:8080/view/'+ id, function(error, resp, body2) {
+		jsonData = JSON.parse(body);
+		jsonData2 = JSON.parse(body2);
+		console.log(jsonData);
+		console.log(jsonData2);
 	response.set("Content-Type", "text/html");
 	response.render('./profile', {
-	})
+		jsonData,
+		jsonData2
+	  });
+	});
+  });
 
 
 });
